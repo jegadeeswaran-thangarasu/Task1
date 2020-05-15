9e0d38eab46e730846f0b6272e67c71b0e798d14
jegadeeswaran-thangarasu <64835787+jegadeeswaran-thangarasu@users.noreply.github.com>
2020-05-15 14:53:54 +0530

    functionality added

--- main.js
+++ main.js
@@ -1,24 +1,58 @@
-let person = {
-    name : [],
-    email : [],
-    phoneNumber : []
-}
+var selectedRow = null;
 
-function addNew() {
-    window.open('addNew.html')
-}
-function formData() {
-    let name = document.getElementById("firstName").value;
-    let email = document.getElementById("email").value;
-    let phoneNumber = document.getElementById("phoneNumber").value;
-    person.name.push(name);
-    person.email.push(email);
-    person.phoneNumber.push(phoneNumber);
-}
-
-function displayEmployeeDetails() {
-    alert('I am DisplayEmployeeDetails() ')
-    for(let i = 0; i < person.name.length; i++) {
-        document.writeln(person.name[i] + "  " + person.email[i] + "  " + person.phoneNumber[i])
+function onFormSubmit() {
+    var formData = readFormData()
+    if(!selectedRow){
+        insertNewRecord(formData);
+    } else {
+        updateRecord(formData);
     }
+    resetForm();
+}
+
+function readFormData() {
+    var formData = {};
+    formData["fullName"] = document.getElementById("fullName").value;
+    formData["emailAddress"] = document.getElementById("emailAddress").value;
+    formData["phoneNumber"] = document.getElementById("phoneNumber").value;
+    return formData;
+}
+
+function insertNewRecord(data) {
+    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.fullName;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.emailAddress;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.phoneNumber;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = `<a onclick="onEdit(this)">Edit</a>
+                       <a onclick="onDelete(this)">Delete</a>`;
+}
+
+function resetForm() {
+    document.getElementById("fullName").value = "";
+    document.getElementById("emailAddress").value = "";
+    document.getElementById("phoneNumber").value = "";
+}
+
+function onEdit(rowData) {
+    selectedRow = rowData.parentElement.parentElement;
+    document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("emailAddress").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("phoneNumber").value = selectedRow.cells[2].innerHTML;
+}
+
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.fullName;
+    selectedRow.cells[1].innerHTML = formData.emailAddress;
+    selectedRow.cells[2].innerHTML = formData.phoneNumber;
+}
+
+function onDelete(rowData) {
+    row = rowData.parentElement.parentElement;
+    document.getElementById("employeeList").deleteRow(row.rowIndex);
+    resetForm();
 }
